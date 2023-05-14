17c5e171a8a255b257afc313b8f8e8372562fa89
DH CHOI <bluetifulc@gmail.com>
2023-05-14 17:21:36 +0900

    Feat: Member 도메인 모델 구현

---
+++ instagram-domain/src/main/java/member/Member.java
@@ -0,0 +1,31 @@
+package member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Member {
+
+	private Long id;
+	private String username;
+	private String password;
+	private String name;
+	private String website;
+	private String introduce;
+	private String email;
+	private String phone;
+	private Gender gender;
+
+	public void updateProfile(UpdateProfileRequest request) {
+		this.username = request.getUsername();
+		this.password = request.getPassword();
+		this.name = request.getName();
+		this.website = request.getWebsite();
+		this.introduce = request.getIntroduce();
+		this.email = request.getEmail();
+		this.phone = request.getPhone();
+		this.gender = request.getGender();
+	}
+
+}

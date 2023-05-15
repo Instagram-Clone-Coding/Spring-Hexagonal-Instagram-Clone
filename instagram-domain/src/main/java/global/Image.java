b112a1405725090b271b64d0b4e6ee122b46cf59
DH CHOI <bluetifulc@gmail.com>
2023-05-15 15:09:28 +0900

    Feature: Image 구현

---
+++ instagram-domain/src/main/java/global/Image.java
@@ -0,0 +1,35 @@
+package global;
+
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+
+	private String imageUrl;
+	private ImageType imageType;
+	private String imageName;
+	private String imageUUID;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.imageUUID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+
+		Image image = (Image)obj;
+		return Objects.equals(this.imageUUID, image.getImageUUID());
+	}
+
+}

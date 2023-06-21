790b58ce4861050cd13cacdd49a7f606e0d6a5e7
DH CHOI <bluetifulc@gmail.com>
2023-06-21 15:56:16 +0900

    Fix: 누락된 파일 추가

---
+++ instagram-adapter/src/test/resources/BaseMember.sql
@@ -0,0 +1,5 @@
+insert into members(username, password, name, email, gender, introduce, phone, website, image_name, image_type, image_uuid, image_url)
+    values('agent_username', 'password', 'agent_name', 'agent@email.com', 'PRIVATE', 'introduce', 'phone', 'website', 'image_name', 'PNG', 'image_uuid', 'image_url');
+
+insert into members(username, password, name, email, gender, introduce, phone, website, image_name, image_type, image_uuid, image_url)
+values('target_username', 'password', 'target_name', 'target@email.com', 'PRIVATE', 'introduce', 'phone', 'website', 'image_name', 'PNG', 'image_uuid', 'image_url');

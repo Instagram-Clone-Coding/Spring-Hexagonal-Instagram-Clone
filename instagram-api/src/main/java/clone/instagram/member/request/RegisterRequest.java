dfce0b8050bfb4a981d5cdd72120ef345a6ec279
DH CHOI <bluetifulc@gmail.com>
2023-06-23 20:48:01 +0900

    Fix: 잘못 지정된 Swagger, 검증 수정

--- instagram-api/src/main/java/clone/instagram/member/request/RegisterRequest.java
+++ instagram-api/src/main/java/clone/instagram/member/request/RegisterRequest.java
@@ -19,15 +19,15 @@ public class RegisterRequest {
 	@Pattern(regexp = "^[0-9a-zA-Z]+$", message = "username엔 대소문자, 숫자만 사용할 수 있습니다.")
 	private String username;
 
-	@ApiModelProperty(value = "이름", example = "이지금", required = true)
-	@NotBlank(message = "이름을 입력해주세요")
-	@Size(min = 2, max = 12, message = "이름은 2문자 이상 12문자 이하여야 합니다")
-	private String password;
-
 	@ApiModelProperty(value = "비밀번호", example = "a12341234", required = true)
 	@NotBlank(message = "비밀번호를 입력해주세요")
 	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "비밀번호는 8자 이상, 최소 하나의 문자와 숫자가 필요합니다")
 	@Size(max = 20, message = "비밀번호는 20문자 이하여야 합니다")
+	private String password;
+
+	@ApiModelProperty(value = "이름", example = "이지금", required = true)
+	@NotBlank(message = "이름을 입력해주세요")
+	@Size(min = 2, max = 12, message = "이름은 2문자 이상 12문자 이하여야 합니다")
 	private String name;
 
 	@ApiModelProperty(value = "이메일", example = "aaa@gmail.com", required = true)
